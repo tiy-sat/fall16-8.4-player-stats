d42f867042f56d1460baf1ccb48ce5673d8c7e77
Jason Vanderslice <jlvanderslice@gmail.com>
2017-01-26 13:26:35 -0600

    Created route for Home at /

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import App from './App'
-import Blog from './Blog'
+import Home from './Home'
 
 render(
   (
     <Router history={hashHistory}>
       <Route component={App}>
+        <Route path="/" component={ Home }/>
       </Route>
     </Router>
   ),
